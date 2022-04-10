d777c80cf927f9201bb3952a43948c5e9f2bc4d5
Nicolas <nicolas.soares.gomes@gmail.com>
2022-04-10 11:15:45 -0300

    fix lint

--- test/fakes/clients/fakeJsonWebTokenClient.ts
+++ test/fakes/clients/fakeJsonWebTokenClient.ts
@@ -1,13 +1,17 @@
-import JsonWebTokenClient from '@src/domain/clients/jsonWebTokenClient'
-import User from '@src/domain/models/user'
+import JsonWebTokenClient, { token } from '@src/domain/clients/jsonWebTokenClient'
 
 class FakeJsonWebTokenClient implements JsonWebTokenClient {
   public async sign (input: object): Promise<string> {
     return 'mockedTest'
   }
 
-  public async verify (token: string, secret: string): Promise<User> {
-    return new User()
+  public async verify (token: string, secret: string): Promise<token> {
+    return {
+      email: 'test@test.com',
+      userId: 1,
+      exp: 1,
+      iat: 1
+    }
   }
 }
 
